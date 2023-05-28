24480e9d6aa268e3264a5109dd14c9d46cb07dd3
Oleh <oleh.liscovych@cloudmade.com>
2023-05-28 19:28:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def greet_user(name):
+    return "Hello, " + name + "! Welcome!"
+
+def greet_kotlin(kotlin_object):
+    kotlin_object.helloFromPython(55)
